f23981a8c8b21f5a2b7ab1eb820902965a6a4f86
Artyom <mr.bartem@mail.ru>
2020-08-29 16:22:27 +0700

    Initial Commit

---
+++ Note/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Note
+//
+//  Created by Артём Бацанов on 29.08.2020.
+//  Copyright © 2020 Артём Бацанов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
