0b0520dc848a23c25c90e56d4a6cfed09128c160
Artyom <mr.bartem@mail.ru>
2020-09-17 12:39:34 +0700

    В разделе "Настройки" реализовал удаление по разделам.

--- Note/Controllers/SettingsViewController.swift
+++ Note/Controllers/SettingsViewController.swift
@@ -30,23 +30,75 @@ class SettingsViewController: UIViewController {
         })
     }
     
+    @IBAction func removeAllNotes() {
+        let alertController = UIAlertController(title: "Очистить корзину?", message: nil, preferredStyle: .alert)
+        
+        let alertAction1 = UIAlertAction(title: "Отмена", style: .default) { (alert) in
+            
+        }
+        
+        let alertAction2 = UIAlertAction(title: "Очистить", style: .cancel) { (alert) in
+            Trash.removeAll()
+        }
+        
+        alertController.addAction(alertAction1)
+        alertController.addAction(alertAction2)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func removeAllTargets() {
+        let alertController = UIAlertController(title: "Удалить все цели?", message: nil, preferredStyle: .alert)
+        
+        let alertAction1 = UIAlertAction(title: "Отмена", style: .default) { (alert) in
+            
+        }
+        
+        let alertAction2 = UIAlertAction(title: "Удалить", style: .cancel) { (alert) in
+            Targets.removeAll()
+        }
+        
+        alertController.addAction(alertAction1)
+        alertController.addAction(alertAction2)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func removeAllTrash() {
+        let alertController = UIAlertController(title: "Удалить все заметки?", message: nil, preferredStyle: .alert)
+        
+        
+        let alertAction1 = UIAlertAction(title: "Отмена", style: .default) { (alert) in
+            
+        }
+        
+        let alertAction2 = UIAlertAction(title: "Удалить", style: .cancel) { (alert) in
+            Notes.removeAll()
+        }
+        
+        alertController.addAction(alertAction1)
+        alertController.addAction(alertAction2)
+        
+        present(alertController, animated: true, completion: nil)
+    }
     
     
     
-//    func scheduleTest() {
-//        let content = UNMutableNotificationContent()
-//        content.title = "Hello World"
-//        content.sound = .default
-//        content.body = "My long body. My long body. My long body. My long body. My long body. My long body. "
-//
-//        let targetDate = Date().addingTimeInterval(10)
-//        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate),   repeats: false)
-//
-//        let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
-//        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
-//            if error != nil {
-//                print("something went wrong")
-//            }
-//        })
-//    }
+    
+    //    func scheduleTest() {
+    //        let content = UNMutableNotificationContent()
+    //        content.title = "Hello World"
+    //        content.sound = .default
+    //        content.body = "My long body. My long body. My long body. My long body. My long body. My long body. "
+    //
+    //        let targetDate = Date().addingTimeInterval(10)
+    //        let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: targetDate),   repeats: false)
+    //
+    //        let request = UNNotificationRequest(identifier: "some_long_id", content: content, trigger: trigger)
+    //        UNUserNotificationCenter.current().add(request, withCompletionHandler: { error in
+    //            if error != nil {
+    //                print("something went wrong")
+    //            }
+    //        })
+    //    }
 }
